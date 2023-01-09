d6b00d6d0c3f3b55a75b99c25c58c5f82f911711
Rodrigo Norys <rodrigo.norys@gmail.com>
2023-01-09 16:42:17 -0300

    Modificações

--- java_projects/src/programs/Arrays.java
+++ java_projects/src/programs/Arrays.java
@@ -7,23 +7,23 @@ public class Arrays {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		// Tamanho do array
+		// Tamanho da matriz
 		System.out.println("Set the array size!");
 		int n = sc.nextInt();
 		
-		// Valores de cada posição do array
+		// Valores de cada posição da matriz
 		System.out.println("Now, define the values of each position.");
 		int[][] mat = new int[n][n];
 		
 		int count = 0;
 		
-		// Estrutura de repetição para ler cada valor do array 
+		// Estrutura de repetição para ler cada posição da matriz 
 		// Para em seguida nos retornar a Diagonal Principal
 		for (int i = 0; i < mat.length; i++) {
 			for (int j = 0; j < mat[i].length; j++) {
 				mat[i][j] = sc.nextInt();
 				
-				// Condição para nos retornar os valores menores que zero
+				// Condição para ler e contabilizar os valores menores que zero
 				if (mat[i][j] < 0) {
 					count++;
 				}
--- java_projects/src/programs/Arrays2.java
+++ java_projects/src/programs/Arrays2.java
@@ -6,45 +6,77 @@ public class Arrays2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		System.out.print("Informe o número de linhas: ");
+		
+		// Número de linhas
+		System.out.print("Input the number of lines: ");
 		int line = sc.nextInt();
-
-		System.out.print("Informe o número de colunas: ");
+		
+		// Número de colunas
+		System.out.print("Input the number of columns: ");
 		int column = sc.nextInt();
+		
+		System.out.println();
 
+		// Informar os valores de cada posição da matriz
+		System.out.println("Input the values for each array position:");
 		Integer[][] matrix = new Integer[line][column];
-
+		
+		// Estrutura para ler cada posição da matriz
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
 				matrix[i][j] = sc.nextInt();
 			}
 		}
-
 		System.out.println();
 
-		System.out.print("Informe um dos números da matriz: ");
+		// Informar um dos valores da matriz
+		System.out.print("Input one of the matrix numbers: ");
 		int matrixNumber = sc.nextInt();
+		
+		// Contador criado somente CASO o matrixNumber não exista dentro da matriz
+		int count = 0;
 
+		// Estrutura para ler novamente cada posição da matriz, com o adicional
+		// Da condição para nos trazer as informações que precisamos
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
-				if (matrix[i][j] == matrixNumber) {
+				
+				// Se matrixNumber for igual a um dos valores dentro da matrix[i][j]...
+				if (matrixNumber == matrix[i][j]) {
+					
+					// Retorna a posição do matrixNumber
 					System.out.println("Position: (" + i + "," + j + ")");
+					
+					// Retorna o valor a esquerda se existir
 					if (j > 0) {
 						System.out.println("Left: " + matrix[i][j - 1]);
+						count++;
 					}
+					
+					// Retorna o valor acima se existir
 					if (i > 0) {
 						System.out.println("Up: " + matrix[i - 1][j]);
+						count++;
 					}
+					
+					// Retorna o valor a direita se existir
 					if (j < matrix[i].length - 1) {
 						System.out.println("Right: " + matrix[i][j + 1]);
+						count++;
 					}
+					
+					// Retorna o valor abaixo se existir
 					if (i < matrix.length - 1) {
 						System.out.println("Down: " + matrix[i + 1][j]);
-					}
-					System.out.println();
+						count++;
+					}			
 				}
 			}
+		}
+		
+		// Para count == 0, nos trará o valor da condição caso seja falsa
+		if (count == 0) {
+			System.out.println("This value isn't inside of array!");
 		}
 		sc.close();
 	}
